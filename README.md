44ec3cefb3d5370ae077575113840b31a506de56
Benny van Zuiden <zuiden@lorentz.leidenuniv.nl>
2015-11-24 12:37:41 +0100

    I'm not a fucking writer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A small and simple header only math parser library:
 /* By BC van Zuiden -- Leiden, November 2015 */
 /* Probably very buggy USE AT OWN RISK this will brick everything you own */
 /* NOBODY but YOU is liable for anything that happened in result of using this */
-/* WARNING: DON'T RUN THIS PROGRAM THIS WILL DESTROY YOUR COMPUTER AND/OR KILOBOTS */
+/* WARNING: DON'T RUN THIS PROGRAM THIS WILL DESTROY YOUR COMPUTER AND/OR HOUSE */
 /* Any copyrighted piece of code within this code is NOT mine */
 /* Inclusion of that code is forced upon me by a scary anonymous guy with a gun*/
 ```
--- pallache.h
+++ pallache.h
@@ -1,9 +1,10 @@
 /* By BC van Zuiden -- Leiden, November 2015 */
 /* Probably very buggy USE AT OWN RISK this will brick everything you own */
 /* NOBODY but YOU is liable for anything that happened in result of using this */
-/* WARNING: DON'T RUN THIS PROGRAM THIS WILL DESTROY YOUR COMPUTER AND/OR KILOBOTS */
+/* WARNING: DON'T RUN THIS PROGRAM THIS WILL DESTROY YOUR COMPUTER AND/OR HOUSE */
 /* Any copyrighted piece of code within this code is NOT mine */
-/* Inclusion of that code is forced upon me by a scary anonymous guy with a gun*/
+/* Inclusion of that code is forced upon me by a scary anonymous guy with a gun */
+/* Thou shalt not remove this comments from this source */
 
 #include <cstdio>
 #include <cstdlib>
